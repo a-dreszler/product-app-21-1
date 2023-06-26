baafcb79181c60251ac44807dcc659180383d128
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-26 15:24:11 +0200

    Utworzenie projektu

---
+++ src/main/java/pl/adreszler/productapp21_1/ProductApp211Application.java
@@ -0,0 +1,13 @@
+package pl.adreszler.productapp21_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApp211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApp211Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/adreszler/productapp21_1/ProductApp211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adreszler.productapp21_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApp211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
