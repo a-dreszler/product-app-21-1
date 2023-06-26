91046baac4d837863ae8f6359fcbae64f02ddb6d
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-26 15:45:27 +0200

    Stworzono szablon strony głównej

--- src/main/java/pl/adreszler/productapp21_1/ProductAppApplication.java
+++ src/main/java/pl/adreszler/productapp21_1/ProductAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductApp211Application {
+public class ProductAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductApp211Application.class, args);
+        SpringApplication.run(ProductAppApplication.class, args);
     }
 
 }
